68125e3e925b31ecdb6303360e6d8a5e5102acac
Administrator <Administrator@V23AVV2NJ2S587U>
2018-11-27 13:46:54 +0800

    1111

--- Healthy/src/main/java/com/dao/VBuyDao.java
+++ Healthy/src/main/java/com/dao/VBuyDao.java
@@ -10,7 +10,7 @@ public interface VBuyDao {
 	public List<VBuy> getAllVbuy(@Param(value="page")int page,@Param(value="rows")int rows);
 	
 	public int findCount();
-	//添加
+	//添加22
 	public boolean VbuySave(VBuy vv);
 	//删除
 	public boolean VbuyDel(@Param(value="id")int id);
