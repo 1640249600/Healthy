889bf845b140fe4b895c9ddf34280a3417dd451c
shi <shimi@LAPTOP-78L70DJ7>
2018-11-27 16:08:27 +0800

    sm

--- Healthy/src/main/java/com/service/DoctorService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import java.util.List;
-
-import com.pojo.doctorlist;
-
-public interface DoctorService {
-	public List<doctorlist> getAllDoctor();
-}
--- Healthy/src/main/java/com/web/controller/VipAction.java
+++ Healthy/src/main/java/com/web/controller/VipAction.java
@@ -5,21 +5,9 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.service.DoctorService;
 
 @Controller
 public class VipAction {
-	@Resource(name="doctorService")
-	private DoctorService doctorService;
-
-	public DoctorService getDoctorService() {
-		return doctorService;
-	}
-
-	public void setDoctorService(DoctorService doctorService) {
-		this.doctorService = doctorService;
-	}
-	
 	
 	@RequestMapping("/login")
 	public String pageCount(){
